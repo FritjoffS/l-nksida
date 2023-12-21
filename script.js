a298c2002e766584c8651776bb6373592d7257b8
FritjoffS <attholmen@gmail.com>
2023-12-21 11:50:13 +0100

    added pages, worked on links and buttons

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // You can add JavaScript code here if needed
 // For example, handling user interactions or making dynamic changes to the page
+
+function openNewWindow(url) {
+    window.open(url, '_blank');
+}
