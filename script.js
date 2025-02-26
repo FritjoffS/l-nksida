8675e9ca83bb3727e1fa470547f96b889265c4c2
FritjoffS <attholmen@gmail.com>
2025-02-26 12:58:24 +0100

    remove comments

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// You can add JavaScript code here if needed
-// For example, handling user interactions or making dynamic changes to the page
+
 
 function openNewWindow(url) {
     window.open(url, '_blank');
