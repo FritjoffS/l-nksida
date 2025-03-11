c5cd499413975b5b58b4b9aa1d520b91b0134bd9
FritjoffS <attholmen@gmail.com>
2025-03-11 16:56:05 +0100

    refactor copyTextToClipboard function to use Clipboard API and enhance user notification display

--- scripts/arbetsbeskr.js
+++ scripts/arbetsbeskr.js
@@ -1,43 +1,36 @@
 
 // Funktion för att kopiera texten
 function copyTextToClipboard(textToCopy) {
-    // Skapa en osynlig textarea för att kopiera texten
-    var textarea = document.createElement("textarea");
-    textarea.value = textToCopy;
-    document.body.appendChild(textarea);
-
-    // Markera och kopiera texten
-    textarea.select();
-    document.execCommand("copy");
-
-    // Ta bort den temporära textarean
-    document.body.removeChild(textarea);
-
-
-    /*alert("Texten har kopierats till urklippet:\n" + textToCopy);*/
-
-    // Skapa ett meddelandeelement
-    var message = document.createElement("div");
-    /*message.textContent = "Texten har kopierats till urklippet:\n" + textToCopy;*/
-    message.textContent = "Texten har kopierats till urklippet";
-    /*message.style.position = "fixed";*/
-    message.style.position = "fixed";
-    message.style.bottom = "10px";
-    message.style.left = "50%";
-    message.style.transform = "translateX(-50%)";
-    /*message.style.backgroundColor = "rgba(51, 51, 51, 0.5)";*/
-    message.style.color = "#fff";
-    message.style.padding = "10px";
-    message.style.borderRadius = "5px";
-    message.style.zIndex = "1000";
-    message.style.fontSize = "48px"; // Ändra storleken på texten
-    document.body.appendChild(message);
-
-    // Ta bort meddelandet efter 3 sekunder
-    setTimeout(function () {
-        document.body.removeChild(message);
-    }, 5000);
-}
+    // Använd Clipboard API för att kopiera texten
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        // Meddela användaren om att texten har kopierats
+        var message = document.createElement("div");
+        message.textContent = "Texten har kopierats till urklippet";
+        message.style.position = "fixed";
+        message.style.bottom = "10px";
+        message.style.left = "50%";
+        message.style.transform = "translateX(-50%)";
+        message.style.color = "#fff";
+        message.style.padding = "10px";
+        message.style.borderRadius = "5px";
+        message.style.zIndex = "1000";
+        message.style.fontSize = "48px";
+        message.style.transition = "opacity 1s";
+        message.style.opacity = "1";
+  
+        document.body.appendChild(message);
+  
+        // Ta bort meddelandet efter 3 sekunder
+        setTimeout(function () {
+            message.style.opacity = "0";
+            setTimeout(function () {
+                document.body.removeChild(message);
+            }, 1000); // Vänta på att övergången ska slutföras
+        }, 3000);
+    }).catch(function(err) {
+        console.error('Kunde inte kopiera texten: ', err);
+    });
+  }
 
 // Lägg till händelselyssnare för varje knapp
 document
