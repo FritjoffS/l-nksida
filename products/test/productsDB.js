a0584cfb1b9403c4deb0b84871513c92941cc82e
FritjoffS <attholmen@gmail.com>
2025-03-12 12:23:31 +0100

    add new product images to enhance visual representation

--- products/test/productsDB.js
+++ products/test/productsDB.js
@@ -3,22 +3,249 @@ const productsDB = {
         {
             name: "Briggs & Stratton",
             subgroups: [
-                { name: "Luftfilter", subtypes: [] },
+                {
+                    name: "Luftfilter",
+                    subtypes: ["Panelfilter", "Ovala Filter", "Runda Filter", "Skumfilter"]
+                },
                 { name: "Bränslefilter", subtypes: [] },
                 { name: "Oljefilter", subtypes: [] },
                 { name: "Tändstift", subtypes: [] },
-                { name: "Packningar", subtypes: [] },
+                {
+                    name: "Packningar",
+                    subtypes: ["Luftrenarpackningar", "Topplockspackningar", "Avgaspackninar"]
+                },
                 { name: "Primerblåsor", subtypes: [] },
             ],
             products: [
                 {
-                    name: "B&S Luftfilter 795073",
+                    name: "B&S Luftfilter 399877SS",
                     subcategory: "Luftfilter",
-                    image: "placeholder-luftfilter.jpg",
+                    subtype: "Panelfilter",
+                    image: "images/399877s.png",
                     info: "Luftfilter för B&S motorer",
-                    productNumber: "795073",
+                    productNumber: "399877S / 4103",
                     specs: {
-                        Dimensioner: "150x100x25 mm"
+                        // Dimensioner: "150x100x25 mm"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 397795S",
+                    subcategory: "Luftfilter",
+                    subtype: "Panelfilter",
+                    image: "images/397795s.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "397795S",
+                    specs: {
+                        Förfilter: "399039"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 794422",
+                    subcategory: "Luftfilter",
+                    subtype: "Panelfilter",
+                    image: "images/794422.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "794422 / 4214",
+                    specs: {
+                        Förfilter: "697015 / 4215"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 794421",
+                    subcategory: "Luftfilter",
+                    subtype: "Panelfilter",
+                    image: "images/794421.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "794421 / 4213",
+                    specs: {
+                        Förfilter: "697292 / 4212"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 499486S",
+                    subcategory: "Luftfilter",
+                    subtype: "Panelfilter",
+                    image: "images/499486S.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "499486S",
+                    specs: {
+                        Förfilter: "273638S"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 792101",
+                    subcategory: "Luftfilter",
+                    subtype: "Panelfilter",
+                    image: "images/792101.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "792101",
+                    specs: {
+                        Förfilter: "273638S"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 593260",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/593260.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "593260",
+                    specs: {
+                        // Förfilter: "273638S"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 792038",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/792038.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "792038 / 4245",
+                    specs: {
+                        Förfilter: "793676 / 4267"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 795066",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/795066.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "795066 / 4240",
+                    specs: {
+                        Förfilter: "796254 / 261"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 697029",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/697029.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "697029 / 4207",
+                    specs: {
+                        Förfilter: "273356S / 4201"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 796970",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/796970.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "796970 / 4238",
+                    specs: {
+                        // Förfilter: "273356S / 4201"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 590601",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/590601.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "590601",
+                    specs: {
+                        Förfilter: "590602"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 496894S",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/496894s.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "496864S / 4139",
+                    specs: {
+                        Förfilter: "272403S / 4142"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 691667",
+                    subcategory: "Luftfilter",
+                    subtype: "Ovala Filter",
+                    image: "images/691667.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "691667 / 4166",
+                    specs: {
+                        Förfilter: "272477S / 4199"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 594201",
+                    subcategory: "Luftfilter",
+                    subtype: "Runda Filter",
+                    image: "images/594201.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "594201 / 4243",
+                    specs: {
+                        // Förfilter: "272477S / 4199"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 591583",
+                    subcategory: "Luftfilter",
+                    subtype: "Runda Filter",
+                    image: "images/591583.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "591583 / 4269",
+                    specs: {
+                        // Förfilter: "272477S / 4199"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 394018S",
+                    subcategory: "Luftfilter",
+                    subtype: "Runda Filter",
+                    image: "images/394018s.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "394018S / 4135",
+                    specs: {
+                        Förfilter: "272490S / 4111"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 692519",
+                    subcategory: "Luftfilter",
+                    subtype: "Runda Filter",
+                    image: "images/692519.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "692519 / 4232",
+                    specs: {
+                        Förfilter: "692520 / 4257"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 698369",
+                    subcategory: "Luftfilter",
+                    subtype: "Skumfilter",
+                    image: "images/698369.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "698369 / 4216",
+                    specs: {
+                        // Förfilter: "692520 / 4257"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 272235S",
+                    subcategory: "Luftfilter",
+                    subtype: "Skumfilter",
+                    image: "images/272235s.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "272235S / 4107",
+                    specs: {
+                        // Förfilter: "692520 / 4257"
+                    }
+                },
+                {
+                    name: "B&S Luftfilter 270579S",
+                    subcategory: "Luftfilter",
+                    subtype: "Skumfilter",
+                    image: "images/270579s.png",
+                    info: "Luftfilter för B&S motorer",
+                    productNumber: "270579S / 4104",
+                    specs: {
+                        // Förfilter: "692520 / 4257"
                     }
                 },
                 {
