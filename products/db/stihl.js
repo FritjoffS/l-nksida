5850f97716a44306e04915e86aa7418dc757a6e6
FritjoffS <attholmen@gmail.com>
2025-03-13 16:47:50 +0100

    update Stihl product details and add new knife model

--- products/db/stihl.js
+++ products/db/stihl.js
@@ -8,26 +8,39 @@ const stihl = {
     ],
     products: [
         {
-            name: "Kniv Imow MI / RMI 4 series 6301-702-010",
+            name: "Kniv Imow MI / RMI 4 series",
             subcategory: "Knivar",
             subtype: "Robotgräsklippare",
-            // image: "placeholder-luftfilter.jpg",
+            image: "images/6301702010.png",
             info: "Standardkniv för Stihl Imow MI/RMI 4 series",
             productNumber: "6301702010",
             specs: {
-                Vårat_Produktnummer: "820515"
+                Vårat_Produktnummer: "820515",
+                Längd: "24 cm"
             }
         },
         {
-            name: "Kniv Imow MI / RMI 6 series 6309-702-0102",
+            name: "Kniv Imow MI / RMI 6 series",
             subcategory: "Knivar",
             subtype: "Robotgräsklippare",
-            // image: "placeholder-luftfilter.jpg",
+            image: "images/63097020102.png",
             info: "Standardkniv för Stihl Imow MI / RMI 6 series",
             productNumber: "63097020102",
             specs: {
                 Vårat_Produktnummer: "820375",
                 Längd: "28 cm"
+            }
+        },
+        {
+            name: "Kniv Imow 5/5EVO, 6/6EVO, 7/7EVO series",
+            subcategory: "Knivar",
+            subtype: "Robotgräsklippare",
+            image: "images/STIA000074201.png",
+            info: "Standardkniv för Stihl Imow 5/5EVO, 6/6EVO, 7/7EVO series 3 st",
+            productNumber: "STIA000074201",
+            specs: {
+                Vårat_Produktnummer: "1096642",
+                Antal: "3 st"
             }
         }
     ]
