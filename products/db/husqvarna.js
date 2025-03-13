e476bc86767bfbeab7adfcbd708f83c83cf411ae
FritjoffS <attholmen@gmail.com>
2025-03-13 09:59:49 +0100

    add husqvarna and stiga product modules to productsDB

---
+++ products/db/husqvarna.js
@@ -0,0 +1,30 @@
+const husqvarna = {
+    name: "Husqvarna",
+    subgroups: [
+        {
+            name: "Knivar",
+            subtypes: ["Automower"]
+        },
+        {
+            name: "Tanklock",
+            subtypes: ["Bränsletank", "Oljetank"] },
+
+        { name: "Tändstift", subtypes: [] }
+    ],
+    products: [
+        {
+            name: "Standardkniv Long Life för Automower 9 st 577864603",
+            subcategory: "Knivar",
+            subtype: "Automower",
+            image: "images/577864603.png",
+            info: "Standardkniv Long Life för Automower",
+            productNumber: "577864603",
+            specs: {
+                // Dimensioner: "200x150x30 mm"
+            }
+        },
+ 
+    ]
+};
+
+export default husqvarna;
--- products/productsDB.js
+++ products/productsDB.js
@@ -1,10 +1,12 @@
 import briggsStratton from './db/briggsStratton.js';
-import stiga from './stiga.js';
+import stiga from './db/stiga.js';
+import husqvarna from './db/husqvarna.js';
 
 const productsDB = {
     categories: [
         briggsStratton,
         stiga,
+        husqvarna,
         // Lägg till fler fabrikat här
     ]
 };
