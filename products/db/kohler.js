ead8f4b0e5a2a5432e278a9da1f757e6b66ee9d0
FritjoffS <attholmen@gmail.com>
2025-03-18 16:33:15 +0100

    feat: add Kohler product data to the database

---
+++ products/db/kohler.js
@@ -0,0 +1,25 @@
+const kohler = {
+    name: "Kohler",
+    subgroups: [
+        {
+            name: "Knivar",
+            subtypes: ["WBH", "Robotgräsklippare", "Rider"]
+        },
+    ],
+    products: [
+        {
+            name: "Kniv Imow 5/5EVO, 6/6EVO, 7/7EVO series",
+            subcategory: "Knivar",
+            subtype: "Robotgräsklippare",
+            image: "images/STIA000074201.png",
+            info: "Standardkniv för Stihl Imow 5/5EVO, 6/6EVO, 7/7EVO series 3 st",
+            productNumber: "STIA000074201",
+            specs: {
+                Vårat_Produktnummer: "1096642",
+                Antal: "3 st"
+            }
+        }
+    ]
+};
+
+export default kohler;
