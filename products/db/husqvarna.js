88f9ce53690b0b51f8bf88b5d62a98e952b9a1de
FritjoffS <attholmen@gmail.com>
2025-03-19 10:51:46 +0100

    feat: add new Husqvarna products and update subtypes in the database

--- products/db/husqvarna.js
+++ products/db/husqvarna.js
@@ -3,7 +3,7 @@ const husqvarna = {
     subgroups: [
         {
             name: "Knivar",
-            subtypes: ["Automower"]
+            subtypes: ["WBH", "Automower", "Tillbehör"]
         },
         {
             name: "Tanklock",
@@ -22,17 +22,67 @@ const husqvarna = {
             productNumber: "577864603",
             specs : {
                 Vårat_Produktnummer: "868983",
+                Fabrikat: "Husqvarna",
+            }
+        },
+        {
+            name: "Mulchewrkniv 53cm",
+            subcategory: "Knivar",
+            subtype: "WBH",
+            image: "images/husqvarna/502881410.webp",
+            info: "MOWER BLADE Mulch 53cm Accessory (with decal) 1 hole, Black",
+            productNumber: "502881410",
+            specs : {
+                Längd: "53 cm",
+                Fabrikat: "Husqvarna",
+            }
+        },
+        {
+            name: "Mulchewrkniv 48cm",
+            subcategory: "Knivar",
+            subtype: "WBH",
+            image: "images/husqvarna/502881310.webp",
+            info: "MOWER BLADE Mulch 48cm Accessory (with decal) 1 hole, Black",
+            productNumber: "502881310",
+            specs : {
+                Längd: "48 cm",
+                Fabrikat: "Husqvarna",
+            }
+        },
+        {
+            name: "Mulchewrkniv Cross-Cut 31cm",
+            subcategory: "Knivar",
+            subtype: "WBH",
+            image: "images/husqvarna/502881710.webp",
+            info: "MOWER BLADE Cross-Cut 31cm Accessory (with decal) Black",
+            productNumber: "502881710",
+            specs : {
+                Längd: "31 cm",
+                Fabrikat: "Husqvarna",
+            }
+        },
+        {
+            name: "Fiberbricka D61",
+            subcategory: "Knivar",
+            subtype: "Tillbehör",
+            image: "images/husqvarna/503259401.webp",
+            info: "Fiberbricka / Friktionsbricka D61",
+            productNumber: "503259401",
+            specs : {
+                Diameter: "61 mm",
+                Fabrikat: "Husqvarna",
             }
         },
         {
             name: "KlippoDäck 502945101",
             subcategory: "Övrigt",
-            // subtype: "Automower",
+            subtype: "WBH",
             image: "images/husqvarna/502945101.png",
             info: "KlippoDäck",
             productNumber: "502945101",
             specs : {
                 Vårat_Produktnummer: "498957",
+                Fabrikat: "Husqvarna",
             }
         },
  
