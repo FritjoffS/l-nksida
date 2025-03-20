d52c3121db15f2f47529afeda59b9a18cf2b0ae1
FritjoffS <attholmen@gmail.com>
2025-03-20 16:06:04 +0100

    feat: add new Luftfilter and Tändstift products to Kohler database

--- products/db/kohler.js
+++ products/db/kohler.js
@@ -2,23 +2,67 @@ const kohler = {
     name: "Kohler",
     subgroups: [
         {
-            name: "Knivar",
-            subtypes: ["WBH", "Rider"]
+            name: "Knivar", subtypes: ["WBH", "Rider"],
+        },
+        {
+            name: "Luftfilter", subtypes: ["Panel typ", "Oval typ", "Rund typ", "Skumfilter", "Förfilter"],
+        },
+        {
+            name: "Tändstift", //subtypes: ["WBH", "Rider"],
+        },
+        {
+            name: "Oljefilter",
         },
     ],
     products: [
         {
-            name: "Kniv",
-            subcategory: "Knivar",
-            subtype: "Rider",
-            image: "images/kohler/1096642.png",
-            info: "Standardkniv för ",
-            productNumber: "",
+            name: "Luftfilter Kohler 1408319S",
+            subcategory: "Luftfilter",
+            subtype: "Panel typ",
+            image: "images/kohler/1408319s.avif",
+            info: "Luftfilter för WBH. Passande Förfilter 1408302S",
+            productNumber: "1408319S",
             specs: {
-                Vårat_Produktnummer: "1096642",
-                Antal: "3 st"
+                Fabrikat: "Kohler",
+                Dimensioner: "145x90x26 mm",
             }
-        }
+        },
+        {
+            name: "Förfilter Kohler 1408302S",
+            subcategory: "Luftfilter",
+            subtype: "Förfilter",
+            image: "images/kohler/1408302s.avif",
+            info: "Förfilter för 1408319S",
+            productNumber: "1408319S",
+            specs: {
+                Fabrikat: "Kohler",
+                // Dimensioner: "145x90x26 mm"
+            }
+        },
+        {
+            name: "Tändstift Kohler 1413211S",
+            subcategory: "Tändstift",
+            // subtype: "Förfilter",
+            image: "images/kohler/1413211s.avif",
+            info: "Tändstift för..",
+            productNumber: "1413211S",
+            specs: {
+                Fabrikat: "Kohler",
+                // Dimensioner: "145x90x26 mm"
+            }
+        },
+        {
+            name: "Tändstift Kohler 1413214S",
+            subcategory: "Tändstift",
+            // subtype: "Förfilter",
+            image: "images/kohler/1413214s.avif",
+            info: "Tändstift för..",
+            productNumber: "1413214S",
+            specs: {
+                Fabrikat: "Kohler",
+                // Dimensioner: "145x90x26 mm"
+            }
+        },
     ]
 };
 
