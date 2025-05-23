559b26607bd932b4115a37817c6a8fec1182bc21
FritjoffS <attholmen@gmail.com>
2025-05-23 10:58:19 +0200

    feat: create initial övriga product category with battery products

---
+++ products/db/övriga.js
@@ -0,0 +1,64 @@
+const övriga = {
+  name: "Övriga",
+  subgroups: [
+    {
+      name: "Batterier", subtypes: ["Rider"],
+    },
+/*     {
+      name: "Luftfilter", subtypes: ["Panel typ", "Oval typ", "Rund typ", "Skumfilter", "Förfilter"],
+    },
+    {
+      name: "Tändstift", //subtypes: ["WBH", "Rider"],
+    },
+    {
+      name: "Oljefilter",
+    }, */
+  ],
+  products: [
+    {
+      name: "Trädgårdsbatteri 12 V 32 Ah U1-32",
+      subcategory: "Batterier",
+      subtype: "Rider",
+      image: "images/övriga/u1-32v.jpg",
+      info: "Trädgårdsbatteri 12 V 32 Ah Plus till vänster",
+      productNumber: "U1-32",
+      specs: {
+        Fabrikat: "Batterilagret",
+        Dimensioner: "196x127x185 mm"
+
+      }
+    },
+        {
+      name: "Trädgårdsbatteri 12 V 32 Ah U1R-32",
+      subcategory: "Batterier",
+      subtype: "Rider",
+      image: "images/övriga/U1R-32.jpg",
+      info: "Trädgårdsbatteri 12 V 32 Ah Plus till höger",
+      productNumber: "U1R-32",
+      specs: {
+        Fabrikat: "Batterilagret",
+        Dimensioner: "196x127x185 mm"
+
+      }
+    },
+            {
+      name: "Trädgårdsbatteri 20 Ah VRLA AGM 12V, T12",
+      subcategory: "Batterier",
+      subtype: "Rider",
+      image: "images/övriga/lp20-12.jpg",
+      info: "Trädgårdsbatteri 12 V 20 Ah Plus till höger",
+      productNumber: "LP20-12",
+      specs: {
+        Fabrikat: "Batterilagret",
+        Dimensioner: "182x77x168 mm"
+
+      }
+    },
+
+
+
+ 
+  ]
+};
+
+export default övriga;
