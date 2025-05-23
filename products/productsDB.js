7dfac4b89c98602884b619776e4c0aba59a2f7e2
FritjoffS <attholmen@gmail.com>
2025-05-23 10:58:13 +0200

    feat: add "Övriga" brand to product categories

--- products/productsDB.js
+++ products/productsDB.js
@@ -3,6 +3,7 @@ import stiga from './db/stiga.js';
 import husqvarna from './db/husqvarna.js';
 import stihl from './db/stihl.js';
 import kohler from './db/kohler.js';
+import övriga from './db/övriga.js';
 
 const productsDB = {
     categories: [
@@ -11,6 +12,7 @@ const productsDB = {
         husqvarna,
         stihl,
         kohler,
+        övriga,
         // Lägg till fler fabrikat här
     ]
 };
