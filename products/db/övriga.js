947698bde1d2f084f0356b85c1e38bf37dfda3dd
FritjoffS <attholmen@gmail.com>
2025-05-23 14:19:57 +0200

    feat: update product images for övriga category with new URLs

--- products/db/övriga.js
+++ products/db/övriga.js
@@ -19,7 +19,7 @@ const övriga = {
       name: "Trädgårdsbatteri 12 V 32 Ah U1-32",
       subcategory: "Batterier",
       subtype: "Rider",
-      image: "images/övriga/u1-32v.jpg",
+      image: "https://www.batterilagret.se/api/artikel/artikelbildviauuid/65d5c13ba3bec781827309_565_477",
       info: "Trädgårdsbatteri 12 V 32 Ah Plus till vänster",
       productNumber: "U1-32",
       specs: {
@@ -32,7 +32,7 @@ const övriga = {
       name: "Trädgårdsbatteri 12 V 32 Ah U1R-32",
       subcategory: "Batterier",
       subtype: "Rider",
-      image: "images/övriga/U1R-32.jpg",
+      image: "https://www.batterilagret.se/api/artikel/artikelbildviauuid/65d5c13bb252e803824814_565_477",
       info: "Trädgårdsbatteri 12 V 32 Ah Plus till höger",
       productNumber: "U1R-32",
       specs: {
@@ -45,7 +45,7 @@ const övriga = {
       name: "Trädgårdsbatteri 20 Ah VRLA AGM 12V, T12",
       subcategory: "Batterier",
       subtype: "Rider",
-      image: "images/övriga/lp20-12.jpg",
+      image: "https://www.batterilagret.se/api/artikel/artikelbildviauuid/65d5c11ab85bd623085818_565_477",
       info: "Trädgårdsbatteri 12 V 20 Ah Plus till höger",
       productNumber: "LP20-12",
       specs: {
