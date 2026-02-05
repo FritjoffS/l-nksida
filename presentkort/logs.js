1d3cf42ec664a00bf89006069acc163d32cbfaa2
FritjoffS <attholmen@gmail.com>
2026-02-05 16:45:51 +0100

    fix: refactor Firebase configuration import in logs.js for improved security

--- presentkort/logs.js
+++ presentkort/logs.js
@@ -3,17 +3,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getDatabase, ref, get, query, orderByChild, limitToLast } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDOFeJ1GvtXtffzCwQFg2M3CCVtpn875KQ",
-    authDomain: "l-nksida.firebaseapp.com",
-    databaseURL: "https://l-nksida-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "l-nksida",
-    storageBucket: "l-nksida.firebasestorage.app",
-    messagingSenderId: "258147870107",
-    appId: "1:258147870107:web:ce637517bc30b7578cad3e"
-};
+import { firebaseConfig } from "../scripts/firebase-config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
