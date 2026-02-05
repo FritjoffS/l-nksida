d55f4c1d919b72d0d8577dd4d48bb75f7f24af75
FritjoffS <attholmen@gmail.com>
2026-02-05 16:45:41 +0100

    fix: refactor Firebase configuration import in listPresentkort.js for improved security

--- presentkort/listPresentkort.js
+++ presentkort/listPresentkort.js
@@ -4,17 +4,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getDatabase, ref, get, set, push, remove, update } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { logAction } from "./logger.js";
-
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDOFeJ1GvtXtffzCwQFg2M3CCVtpn875KQ",
-    authDomain: "l-nksida.firebaseapp.com",
-    databaseURL: "https://l-nksida-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "l-nksida",
-    storageBucket: "l-nksida.firebasestorage.app",
-    messagingSenderId: "258147870107",
-    appId: "1:258147870107:web:ce637517bc30b7578cad3e"
-};
+import { firebaseConfig } from "../scripts/firebase-config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
